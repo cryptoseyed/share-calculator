3f25f870473d3d4f5536b73dd91bbc3d3d73f853
CryptoSeyed <cryptoseyed@gmail.com>
2018-11-30 17:02:47 +0330

    1. Fixes: A. Fix calculate_credit() for PPLNS system B. Fix pay_payments() to be consistent with calculate_credit()
    2. Delete unused functions, classes and variables

--- src/share_calculator.py
+++ src/share_calculator.py
@@ -6,9 +6,6 @@ import json
 from os import urandom
 from binascii import hexlify
 
-from operator import itemgetter
-from itertools import groupby
-
 import requests
 
 import psycopg2
@@ -24,27 +21,16 @@ colorama_init(autoreset=True)
 
 SLEEP_TIME = SETTING['SLEEP_TIME']
 BLOCK_REWARD = SETTING['BLOCK_REWARD']
-POOL_FEE = SETTING['POOL_FEE']
+POOL_FEE = SETTING['POOL_FEE'] # In %
 SG_WALLET_RPC_ADDR = SETTING['SG_WALLET_RPC_ADDR_TESTNET']
 TG_WALLET_RPC_AUTH = SETTING['TG_WALLET_RPC_AUTH_TESTNET']
-SG_DAEMON_ADDR = SETTING['SG_DAEMON_ADDR_TESTNET']
 CHANGE_STATUS_TO_SUCCESS_LIMIT = SETTING['CHANGE_STATUS_TO_SUCCESS_LIMIT']
 WALLET_NAME = SETTING['WALLET_NAME']
 TESTING_MODE = SETTING['TESTING_MODE']
 PSQL_USERNAME = SETTING['psqlUser']
 PSQL_PASSWORD = SETTING['psqlPass']
 N = SETTING['N']
-
-class E_V_S:
-	def __init__(self):
-		self.expexted_valid_shares = 0
-	def get(self):
-		return self.expexted_valid_shares
-	def add(self):
-		self.expexted_valid_shares += N
-
-EXPECTED_VALID_SHARES = E_V_S()
-
+LAST_BLOCK = 0
 
 def message(string):
 	string = str(string)
@@ -143,27 +129,6 @@ def get_block_status(cur):
 
 	return changed_status_to_3_blocks
 
-def daemon(s_method, d_params=None):
-	d_headers = {'Content-Type': 'application/json'}
-	d_daemon_input = {"jsonrpc": "2.0", "id": "0", "method" :  s_method}
-
-	if d_params is not None:
-		d_daemon_input['params'] = d_params
-
-	o_rsp = requests.post('http://' + SG_DAEMON_ADDR + '/json_rpc',
-							data=json.dumps(d_daemon_input),
-							headers=d_headers,
-							timeout=60.0)  #Wallet can be fairly slow for large requests
-
-	if o_rsp.status_code != requests.codes.ok: # pylint: disable=maybe-no-member
-		raise RuntimeError('HTTP Request error : ' + o_rsp.reason)
-
-	d_jsn = o_rsp.json()
-	if 'error' in d_jsn:
-		raise RuntimeError("Wallet error: " + d_jsn['error']['message'])
-
-	return d_jsn['result']
-
 def wallet_rpc(s_method, d_params=None):
 	d_headers = {'Content-Type': 'application/json'}
 	d_rpc_input = {"jsonrpc": "2.0", "id": "0", "method" :  s_method}
@@ -185,24 +150,6 @@ def wallet_rpc(s_method, d_params=None):
 		raise RuntimeError("Wallet error: " + d_jsn['error']['message'])
 
 	return d_jsn['result']
-
-def valid_shares_between_block(cur, height):
-	prev_time = 0
-	if height != 0:
-		cur.execute('SELECT time FROM mined_blocks WHERE height=%s', (height - 1, ))
-		prev_time = cur.fetchone()[0]
-
-	cur.execute('SELECT time FROM mined_blocks WHERE height=%s', (height, ))
-	cure_time = cur.fetchone()[0]
-
-	cur.execute('SELECT * FROM valid_shares WHERE time BETWEEN %s AND %s',
-					(prev_time + 1, cure_time))
-
-	return cur.fetchall()
-
-def get_block_id(cur, height):
-	cur.execute('SELECT blk_id from mined_blocks WHERE height=%s', (height, ))
-	return cur.fetchone()[0]
 
 def get_user_wallet(cur, uid):
 	cur.execute('SELECT wallet FROM users WHERE uid=%s', (uid, ))
@@ -226,22 +173,22 @@ def get_balances_and_thresholds(cur):
 						cuid AS uid,
 						creRes.creSum,
 						COALESCE(payRes.paySum, 0) AS paySum
-					FROM (SELECT
-						COALESCE(SUM(amount), 0) AS creSum,
-						uid AS cuid
-					FROM wpv1.credits
-					GROUP BY cuid) AS creRes
-					LEFT JOIN (SELECT
-						COALESCE(SUM(amount), 0) AS paySum,
-						uid AS puid
-					FROM wpv1.payments
-					GROUP BY puid) AS payRes
+						FROM (SELECT
+									COALESCE(SUM(amount), 0) AS creSum,
+									uid AS cuid
+								FROM wpv1.credits
+								GROUP BY cuid) AS creRes
+						LEFT JOIN (SELECT
+										COALESCE(SUM(amount), 0) AS paySum,
+										uid AS puid
+									FROM wpv1.payments
+									GROUP BY puid) AS payRes
 						ON puid = cuid) AS balance
 					LEFT JOIN (SELECT
-						uid AS tuid,
-						payment_threshold
-					FROM users) AS payTh
-						ON tuid = uid""")
+									uid AS tuid,
+									payment_threshold
+								FROM users) AS payTh
+									ON tuid = uid""")
 
 	results = cur.fetchall()
 	return_value = []
@@ -250,17 +197,6 @@ def get_balances_and_thresholds(cur):
 		return_value.append([result[0], result[1] - result[2], result[3]])
 
 	return return_value
-
-def get_new_payments(cur):
-	balances_and_thresholds = get_balances_and_thresholds(cur)
-
-	new_payments = []
-
-	for item in balances_and_thresholds:
-		if item[1] - item[2] >= 0:
-			new_payments.append([item[0], int(item[1] / item[2]) * item[2]])
-
-	return new_payments
 
 def update_status(cur, txid, status):
 	cur.execute("UPDATE payments SET status = '%s' WHERE txid = %s", (status, txid))
@@ -294,170 +230,123 @@ def check_payment_status(cur):
 
 	message('Change status to success in height ' + str(current_block_height) + ' completed')
 
-def get_last_total_valid_shares(cur, changed_blocks):
-	return_value = 0
-	for block in changed_blocks:
-		return_value += valid_shares_between_block(cur, block)
-	return return_value
-
-def make_valid_ranges(cur, block_times):
-	ret_val = []
-	for item in block_times:
-		cur.execute("""SELECT
-							MAX(time) + 1 AS lower_bound
-						FROM mined_blocks
-						WHERE time < %s;""", (item[0],))
+def make_N_shares(cur):
+	flag = False
+	limiter = 1
+	while flag is False:
+		cur.execute("""SELECT COALESCE(SUM(temp2.sum), 0) AS sum
+						FROM
+							(SELECT uid, SUM(count) 
+							FROM
+								(SELECT uid, count 
+								FROM wpv1.valid_shares 
+								ORDER BY TIME DESC LIMIT %s) AS temp 
+							GROUP BY uid) AS temp2""", (limiter,))
 		result = cur.fetchone()
-		if result[0] == None:
-			ret_val.append([{'lower': 0, 'upper': item[0]}])
+		if result[0] >= N:
+			flag = True
 		else:
-			ret_val.append([{'lower': result[0], 'upper': item[0]}])
-	return ret_val
-
-def get_valid_blocks_to_pay_credit(cur):
-	cur.execute("""SELECT
-					blk_id
-					FROM mined_blocks
-					WHERE status = 3;""")
+			limiter += 1
+	
+	cur.execute("""SELECT uid, SUM(count) 
+					FROM
+						(SELECT uid, count 
+						FROM wpv1.valid_shares 
+						ORDER BY TIME DESC LIMIT %s) AS temp 
+					GROUP BY uid""")
+	
 	result = cur.fetchall()
-	if result == []:
-		return []
 
-	return result
+	counter = 0
 
-def block_timestamp(cur, blk_id):
-	cur.execute("""SELECT time
-					FROM mined_blocks
-					WHERE blk_id=%s""", (blk_id,))
-	upper_result = cur.fetchone()
+	retval = {}
 
-	cur.execute("""SELECT time
-					FROM mined_blocks
-					WHERE blk_id=%s""", (blk_id-1,))
-	lower_result = cur.fetchone()
-
-	result = {'upper': upper_result[0], 'lower': 0}
-	if lower_result != None:
-		result['lower'] = lower_result[0]
-
-	return result
-
-def check_expected_valid_shares(cur, blocks):
-	count = 0
-	for b in blocks:
-		timestamp = block_timestamp(cur, b)
-		cur.execute("""SELECT COALESCE(sum(COUNT), 0)
-						FROM valid_shares
-						WHERE TIME BETWEEN %s AND %s;""", (timestamp['lower'], timestamp['upper']))
-		result = cur.fetchone()
-
-		count += result[0]
-
-	retval = False
-	if count >= N:
-		retval = True
+	for i in result:
+		if counter + i[1] > N:
+			retval[str(i[0])] = (counter + i[1]) - N
+		else:
+			retval[i[0]] = i[1]
+			counter += i[1]
+	
 	return retval
 
+def get_max_block_id(cur):
+	cur.execute("""SELECT MAX(blk_id) FROM mined_blocks""")
+	return cur.fetchone()[0]
+
 def calculate_credit(cur):
-	blocks = get_valid_blocks_to_pay_credit(cur)
-	if check_expected_valid_shares(cur, blocks):
-		pass # Calculate payments
+	# Credits must calculate here and then pay_payments should do its work there
+	blk_id = get_max_block_id(cur)
 
-	return
+	N_shares = make_N_shares(cur)
 
-	# last_total_valid_shares = get_last_total_valid_shares(cur, changed_blocks)
-	# if int(( last_total_valid_shares) / N) < 1:
-	# 	return
+	for uid in N_shares:
+		record_credit(cur, blk_id, uid, N_shares[uid])
 
-	# user_credits = []
-	# for height in changed_blocks:
-	# 	user_total_valid_share_in_block = []
+def pay_payments(cur):
+	destinations = []
+	messages = {}
+	new_payments = {}
 
-	# 	valid_shares = valid_shares_between_block(cur, height)
+	user_payment_info = get_balances_and_thresholds(cur)
 
-	# 	valid_shares.sort(key=lambda x: int(x[1]))
+	for i in user_payment_info:
+		if i[1] >= i[2]:
+			new_payments[i[0]] = i[1]
 
-	# 	total_valid_share_in_block = 0
+	for uid in new_payments:
+		user_wallet = get_user_wallet(cur, uid)
 
-	# 	for elt, items in groupby(valid_shares, itemgetter(1)):
-	# 		user_valid_shares_count = 0
-	# 		for i in items:
-	# 			user_valid_shares_count += int(i[3])
-	# 		total_valid_share_in_block += user_valid_shares_count
-	# 		temp = next((item for item in user_total_valid_share_in_block if item['uid'] == elt), None)
-
-	# 		if temp is None:
-	# 			user_total_valid_share_in_block.append({'uid': elt, 'valid_shares': user_valid_shares_count})
-	# 		else:
-	# 			temp['valid_shares'] += user_valid_shares_count
-
-	# 	for i, _ in enumerate(user_total_valid_share_in_block):
-	# 		credit_temp = user_total_valid_share_in_block[i]['valid_shares'] * \
-	# 		(BLOCK_REWARD * (1 - POOL_FEE) / total_valid_share_in_block)
-
-	# 		temp = next((item for item in user_credits if item['uid'] == elt), None)
-
-	# 		if temp == None:
-	# 			user_credits.append({'uid': user_total_valid_share_in_block[i]['uid'], 'credit': credit_temp})
-	# 		else:
-	# 			temp['credit'] += credit_temp
-
-	# 	message('Block ' + str(height) + ' valid shares calculated')
-	# 	update_block_status(cur, height, 4)
-
-	# blk_id = get_block_id(cur, height)
-	# for user in user_credits:
-	# 	record_credit(cur, blk_id, user['uid'],
-	# 							user['credit'])
-
-def pay_payments(cur, new_payments):
-	valid_payment_message = False
-
-	for new_payment in new_payments:
-		valid_payment_message = True
-
-		destinations = []
-
-		if new_payment != 0:
-
-			user_wallet = get_user_wallet(cur, new_payment[0])
-
-			destinations.append({'amount': new_payment[1],
+		amount = (BLOCK_REWARD*(new_payments[uid]/N))*(1-(POOL_FEE/100))
+		destinations.append({'amount': amount,
 								'address': user_wallet})
+		messages[uid] = {'amount': amount, 'address': user_wallet}
 
-			if destinations != []:
-				json_data = {}
-				transfer_info = {}
+	if destinations != []:
+		json_data = {}
+		transfer_info = {}
 
-				txid = hexlify(urandom(32)).decode()
+		txid = hexlify(urandom(32)).decode()
 
-				if TESTING_MODE is True:
-					json_data['tx_hash'] = 'TEST'
-					transfer_info['transfer'] = {}
-					transfer_info['transfer']['timestamp'] = 1536234479
-				else:
-					json_data = wallet_rpc('transfer',
-											{'destinations': destinations, 'payment_id': txid}) # 'get_tx_key': True
-					transfer_info = wallet_rpc('get_transfer_by_txid', {'txid': json_data['tx_hash']})
+		if TESTING_MODE is True:
+			json_data['tx_hash'] = 'TEST'
+			transfer_info['transfer'] = {}
+			transfer_info['transfer']['timestamp'] = 1536234479
+		else:
+			json_data = wallet_rpc('transfer',
+									{'destinations': destinations, 'payment_id': txid}) # 'get_tx_key': True
+			transfer_info = wallet_rpc('get_transfer_by_txid', {'txid': json_data['tx_hash']})
 
-				submit_payment(cur, new_payment[0], new_payment[1], json_data['tx_hash'],
-								transfer_info['transfer']['timestamp'])
 
-				message('Pay ' + str(format(int(destinations[0]['amount'])/1000000000, '.9f')) + \
-						' to ' + str(destinations[0]['address']))
 
-	if valid_payment_message is True:
+		for uid in messages:
+			submit_payment(cur, uid, messages[uid]['amount'], json_data['tx_hash'],
+						transfer_info['transfer']['timestamp'])
+
+			message('Pay ' + str(format(int(messages[uid]['amount'])/1000000000, '.9f')) + \
+					' to ' + str(messages[uid]['address']))
+
 		message('Payments completed')
 
 def get_wallet_hight():
 	return wallet_rpc('getheight')['height']
+
+def wait_until_new_block(cur):
+	result = LAST_BLOCK
+	while result == LAST_BLOCK:
+		cur.execute("""SELECT MAX(blk_id) FROM mined_blocks""")
+		result = cur.fetchone[0]
+
+		time.sleep(SLEEP_TIME/2)
+
+	result = LAST_BLOCK
 
 try:
 	CONN = None
 	CURS = None
 	CONN, CURS = connection_init()
 
-	database_init(CURS, CONN);input();
+	database_init(CURS, CONN)
 
 	wallet_rpc('open_wallet', {'filename': WALLET_NAME, 'password': ''})
 
@@ -467,17 +356,15 @@ try:
 
 		check_payment_status(CURS)
 
-		CHANGED_BLOCKS = get_block_status(CURS)
+		get_block_status(CURS)
 
-		# calculate_credit(CURS, CHANGED_BLOCKS)
+		calculate_credit(CURS)
 
-		NEW_PAYMENTS = get_new_payments(CURS)
-
-		pay_payments(CURS, NEW_PAYMENTS)
+		pay_payments(CURS)
 
 		print()
 
-		time.sleep(SLEEP_TIME)
+		wait_until_new_block(CURS)
 
 except KeyboardInterrupt:
 	print()
